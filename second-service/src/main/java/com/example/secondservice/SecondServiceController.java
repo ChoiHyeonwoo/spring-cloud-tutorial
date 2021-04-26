20f449e32e447cdff024b8e774ca0a424e5b8a46
hyeonwoo <escape0619@gmail.com>
2021-04-26 21:09:37 +0900

    @Configuration을 통한 filter 적용

--- second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,15 +1,24 @@
 package com.example.secondservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/second-service")
+@Slf4j
 public class SecondServiceController {
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to the second service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return "Hello World in Second Service";
     }
 
 }

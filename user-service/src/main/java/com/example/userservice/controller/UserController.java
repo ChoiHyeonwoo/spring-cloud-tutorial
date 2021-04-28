cb7cf2fe935d687a18df2c95e9c24885c86b533f
hyeonwoo <escape0619@gmail.com>
2021-04-29 07:53:19 +0900

    user service setting

---
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.vo.Greeting;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final Greeting greeting;
+
+    @GetMapping("/health_check")
+    public String status(){
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return greeting.getMessage();
+    }
+}
